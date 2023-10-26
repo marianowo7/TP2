0d274875220c52c8887d72364041d72b0be3a689
marianowo7 <105328355+marianowo7@users.noreply.github.com>
2023-10-26 16:38:28 -0300

    tp3

--- TP2.py
+++ TP2.py
@@ -7,78 +7,96 @@
 
 def main():
     porcentaje_iva = 1.21
-    global nombre  
-    global valor
-    global vof
     nombre = ""
     valor = 0
     vof = False
 
-    def ingresarNombre(): 
-      nombreProducto = str(input( "Ingrese el nombre del producto : ",)) #No recibe parámetro y retorna valor
-      return nombreProducto
-      
-    def ingresarValor(): 
-      valorProducto = int(input("Ingrese el valor del producto (sin comas) : ")) #No recibe parámetro y retorna valor
-      return valorProducto
-    
-    def obtenerDatosProducto():  ## No recibe parámetro y no retorna valor
-        global nombre  
-        global valor
-        global vof
+    lista_mouse = ["Logitech G pro", "Logitech G203", "Razer Deathadder V2", "Razer Naga", "Redragon M711", "Redragon M808"]
+    lista_mouse_precios = [70, 50, 80, 85, 40, 45]
+    lista_teclado = ["Logitech K400", "Logitech K480", "Razer Blackwidow", "Razer Hunstman", "Redragon Kumara k552", "Redragon Visnu"]
+    lista_teclado_precios = [40, 50, 120, 185, 90, 75]
+    lista_auricular = ["Logitech G335", "Logitech G435", "Razer Blackshark", "Razer kraken", "Redragon H120", "Redragon H220"]
+    lista_auricular_precios = [140, 150, 100, 95, 75, 60]
 
-        nombreProducto = ingresarNombre()
+    productos_carrito = []
+    precio_carrito = []
 
-        valorProducto = ingresarValor()
+    def obtenerDatosProducto():  # No recibe parámetro y no retorna valor
+        cantidad_productos = int(input("Cuantos productos desea comprar? : ", ))
 
-        if valorProducto < 0 or nombreProducto == "":
-            response_menos_que_cero = str(
-                input("El valor ingresado no puede ser cero o menor que cero / el nombre del producto no puede estar vacio, presione 1 para reiniciar el programa : " )
-            )
-            if response_menos_que_cero == "1":
-                main()
+        for i in range(cantidad_productos):
 
-        pagar_en_cuotas = input("Desea pagar en cuotas? (si o no) : ",)
-        if (pagar_en_cuotas == "si" or pagar_en_cuotas == "SI"or pagar_en_cuotas == "Si"):
-            pagoEnCuotas(nombreProducto, valorProducto)
-            nombre = nombreProducto
-            valor = valorProducto
-            vof = True
-        else:
-            nombre = nombreProducto
-            valor = valorProducto
-            vof = False
+            eleccion_productos = int(input("Ingrese el tipo de producto que desea comprar: 1. Mouse, 2. Teclado, 3. Auricular : "))
+            if eleccion_productos == 1:
+                i = 0
+                int(i)
+                for mouse in lista_mouse:
+                    print( str(i) + ". " + mouse)
+                    int(i)
+                    i = i+1
+                num_producto = int(input("Escriba el numero del producto que desea comprar : ", ))
+                productos_carrito.append(lista_mouse[num_producto])
+                precio_carrito.append(lista_mouse_precios[num_producto])
+                print(productos_carrito)
+                print(precio_carrito)
+            elif eleccion_productos == 2:
+                i = 0
+                int(i)
+                for teclado in lista_teclado:
+                    print( str(i) + ". " + teclado)
+                    int(i)
+                    i = i+1
+                num_producto = int(input("Escriba el número del producto que desea comprar : ", ))
+                productos_carrito.append(lista_teclado[num_producto])
+                precio_carrito.append(lista_teclado_precios[num_producto])
+                print(productos_carrito)
+                print(precio_carrito)
+            elif eleccion_productos == 3:
+                i = 0
+                int(i)
+                for auricular in lista_auricular:
+                    print( str(i) + ". " + auricular)
+                    int(i)
+                    i = i+1
+                num_producto = int(input("Escriba el numero del producto que desea comprar : ", ))
+                productos_carrito.append(lista_auricular[num_producto])
+                precio_carrito.append(lista_auricular_precios[num_producto])
+                print(productos_carrito)
+                print(precio_carrito)
+                
+
+            pagar_en_cuotas = input("Desea pagar en cuotas? (si o no) : ",)
+            if (pagar_en_cuotas == "si" or pagar_en_cuotas == "SI"or pagar_en_cuotas == "Si"):
+                pagoEnCuotas(productos_carrito, precio_carrito)
+            # else:
+            #     nombre = nombreProducto
+            #     valor = valorProducto
+            #     vof = False
             
-    def pagoEnCuotas(nombre, valor): ## Recibe parametro y no retorna valor
+    def pagoEnCuotas(productos, valor_productos): # Recibe parametro y no retorna valor
         cuantas_cuotas = int( input("desea pagar en 3, 6 , 12 cuotas?(Ingresar numero de cuotas) : ", ))
 
-        if cuantas_cuotas == 3:
-            valor_total_en_tres_cuotas = round(
-                valor * 1.2994 * porcentaje_iva
-            )
-            valor_en_tres_cuotas = round(valor_total_en_tres_cuotas / 3)
-            print("")
-            print("Usted compró " + str(nombre))
-            print("En tres cuotas de " + str(valor_en_tres_cuotas) + "$")
-            print("(Total de " + str(valor_total_en_tres_cuotas) + "$)")
-        elif cuantas_cuotas == 6:
-            valor_total_en_tres_cuotas = round(
-                valor * 1.5412 * porcentaje_iva
-            )
-            valor_en_tres_cuotas = round(valor_total_en_tres_cuotas / 6)
-            print("")
-            print("Usted compró " + str(nombre))
-            print("En tres cuotas de " + str(valor_en_tres_cuotas) + "$")
-            print("(Total de " + str(valor_total_en_tres_cuotas) + "$)")
-        elif cuantas_cuotas == 12:
-            valor_total_en_tres_cuotas = round(
-                valor * 2.04101 * porcentaje_iva
-            )
-            valor_en_tres_cuotas = round(valor_total_en_tres_cuotas / 12)
-            print("")
-            print("Usted compró " + str(nombre))
-            print("En tres cuotas de " + str(valor_en_tres_cuotas) + "$")
-            print("(Total de " + str(valor_total_en_tres_cuotas) + "$)")
+        # if cuantas_cuotas == 3:
+        #     valor_total_en_tres_cuotas = round(valor * 1.2994 * porcentaje_iva)
+        #     valor_en_tres_cuotas = round(valor_total_en_tres_cuotas / 3)
+        #     print("")
+        #     print("Usted compró " + str(nombre))
+        #     print("En tres cuotas de " + str(valor_en_tres_cuotas) + "$")
+        #     print("(Total de " + str(valor_total_en_tres_cuotas) + "$)")
+        # elif cuantas_cuotas == 6:
+        #     valor_total_en_tres_cuotas = round(valor * 1.5412 * porcentaje_iva)
+        #     valor_en_tres_cuotas = round(valor_total_en_tres_cuotas / 6)
+        #     print("")
+        #     print("Usted compró " + str(nombre))
+        #     print("En tres cuotas de " + str(valor_en_tres_cuotas) + "$")
+        #     print("(Total de " + str(valor_total_en_tres_cuotas) + "$)")
+        # elif cuantas_cuotas == 12:
+        #     valor_total_en_tres_cuotas = round(valor * 2.04101 * porcentaje_iva)
+        #     valor_en_tres_cuotas = round(valor_total_en_tres_cuotas / 12)
+        #     print("")
+        #     print("Usted compró " + str(nombre))
+        #     print("En tres cuotas de " + str(valor_en_tres_cuotas) + "$")
+        #     print("(Total de " + str(valor_total_en_tres_cuotas) + "$)")
    
     def unPago(nombre, valor, vof): # Recibe parámetro y retorna valor
         cuotas = vof
@@ -92,7 +110,7 @@ def main():
    
     obtenerDatosProducto()
 
-    pago = unPago(nombre, valor, vof)
-    print(pago)
+    # pago = unPago(nombre, valor, vof)
+    # print(pago)
     
 main()
